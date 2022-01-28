3869cf094dfa727d9fd5e7f238e12c7a14e998c3
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-28 12:37:17 -0500

    Basic intro to c#

---
+++ Basic_C#_Programs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your Name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello \"" + name +"\"");
+            Console.Read();
+        }
+    }
+}
