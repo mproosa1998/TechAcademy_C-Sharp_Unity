27dd9c7c338f115bbf20fec9dba0e06b6d025b0e
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-31 10:10:13 -0500

    Boolean Logic assignment

---
+++ Basic_C#_Programs/BooleanLogic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            bool boolDUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qualified for car insurance?");
+            bool qualified = age > 15 && !boolDUI && speedingTickets < 3;
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
