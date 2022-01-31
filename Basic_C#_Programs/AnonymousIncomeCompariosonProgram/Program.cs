4ba05b6e72bd44c063cfb078696e4185b6aa2565
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-31 09:41:17 -0500

    Math and comparison operators assignment

---
+++ Basic_C#_Programs/AnonymousIncomeCompariosonProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AnonymousIncomeCompariosonProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program\n");
+            Console.WriteLine("Person 1");
+            Console.Write("Hourly Rate: ");
+            float person1HourlyRate = float.Parse(Console.ReadLine());
+            Console.Write("Hours Worked Per Week: ");
+            float person1HoursWorked = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+            Console.Write("Hourly Rate: ");
+            float person2HourlyRate = float.Parse(Console.ReadLine());
+            Console.Write("Hours Worked Per Week: ");
+            float person2HoursWorked = float.Parse(Console.ReadLine());
+
+            float weeksWorked = 50;
+            float Salary1 = person1HourlyRate * person1HoursWorked * weeksWorked;
+            float Salary2 = person2HourlyRate * person2HoursWorked * weeksWorked;
+            bool person1_GreaterThan_person2 = Salary1 > Salary2;
+
+            Console.WriteLine("Annual Salary of Person 1: " + Salary1);
+            Console.WriteLine("Annual Salary of Person 2: " + Salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(person1_GreaterThan_person2);
+            Console.Read();
+        }
+    }
+}
