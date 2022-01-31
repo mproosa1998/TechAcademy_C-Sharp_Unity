622b00ac82f84a9e3caed6934953c38a5bd5f5f2
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-31 10:59:22 -0500

    while do while assignment

---
+++ Basic_C#_Programs/whileDoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace whileDoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool escapeWhile = false;
+            while (!escapeWhile)
+            {
+                do
+                {
+                    Console.WriteLine("To exit loop type: ENTER");
+                    if (Console.ReadLine() == "ENTER")
+                    {
+                        escapeWhile = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Loop Continuing");
+                    }
+                } while (!escapeWhile);
+            }
+        }
+    }
+}
