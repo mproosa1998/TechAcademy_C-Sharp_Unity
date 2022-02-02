d90fbea47636968b2e9a98b7f56d601bf8b61743
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-02-02 09:44:31 -0500

    MethodAssignment3

---
+++ Basic_C#_Programs/MethodAssignment3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    class Class1
+    {
+        public static void MathIsFun(int first, int second)
+        {
+            Console.WriteLine(first * second);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.MathIsFun(4, 5);
+            Class1.MathIsFun(first: 20034, second: 42);
+            Console.ReadLine();
+        }
+    }
+}
