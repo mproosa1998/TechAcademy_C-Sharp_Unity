61e095b0bfe53dfb4a29c4863b4c079cba65c895
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-02-02 09:19:55 -0500

    MainMethodSubmission Assignment step 218

---
+++ Basic_C#_Programs/MainMethodAssignment1/Mathy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment1
+{
+    class Mathy
+    {
+        public static int letsMath(int integer)
+        {
+            return integer + integer;
+        }
+        public static int letsMath(decimal deceger)
+        {
+            return Convert.ToInt32(50/deceger);
+        }
+        public static int letsMath(string strinteger)
+        {
+            return 10 * Convert.ToInt32(strinteger);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mathy.letsMath(10));
+            Console.WriteLine(Mathy.letsMath(.5m));
+            Console.WriteLine(Mathy.letsMath("4"));
+            Console.ReadLine();
+        }
+    }
+}
