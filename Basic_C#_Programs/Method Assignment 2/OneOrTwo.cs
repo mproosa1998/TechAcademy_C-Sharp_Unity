8bd97d755b83e2a246c5ae5e6131e86d92596cb2
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-02-02 09:38:22 -0500

    MethodAssignment2

---
+++ Basic_C#_Programs/Method Assignment 2/OneOrTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment_2
+{
+    class OneOrTwo
+    {
+        public static void OneOrTwoWhat(int One, int Two = 2)
+        {
+            
+            Console.WriteLine(One * Two);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Method Assignment 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer:");
+            int One = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer or press ENTER:");
+            string input = Console.ReadLine();
+            if (input != "")
+            {
+                int Two = Convert.ToInt32(input);
+                OneOrTwo.OneOrTwoWhat(One, Two);
+            }
+            else
+            {
+                OneOrTwo.OneOrTwoWhat(One);
+            }
+            Console.ReadLine();
+        }
+    }
+}
