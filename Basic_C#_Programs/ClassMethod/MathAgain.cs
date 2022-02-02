54f850bf688f8f3fd00717782c7d77a21819832b
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-02-02 10:17:11 -0500

    Class Method Assignment

---
+++ Basic_C#_Programs/ClassMethod/MathAgain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class MathAgain
+    {
+        public void mathy(int One)
+        {
+            Console.WriteLine(One / 2);
+        }
+
+        public static void mathy(int One, out int Solution, int Two = 2)
+        {
+            Solution = One / Two;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/ClassMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathAgain newMath = new MathAgain();
+            Console.WriteLine("Enter Integer:");
+            int One = Convert.ToInt32(Console.ReadLine());
+            newMath.mathy(One);
+            int answer;
+            MathAgain.mathy(3,out answer,2);
+            Console.WriteLine(answer);
+
+            Console.Read();
+        }
+    }
+}
