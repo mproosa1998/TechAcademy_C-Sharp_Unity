270bf252f01dde30f2534e3043202992a63a2028
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-02-04 10:21:49 -0500

    Abstraction Assignment

---
+++ Basic_C#_Programs/AbstractAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
