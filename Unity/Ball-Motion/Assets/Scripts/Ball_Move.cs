52af1ac5ac384e24c8a852a50257b12e41d9d09d
mroosa1998 <mproosa1998@gmail.com>
2022-02-17 14:11:24 -0500

    Maze

---
+++ Unity/Ball-Motion/Assets/Scripts/Ball_Move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Move : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+        rigb.AddForce(movement * speed);
+    }
+}
